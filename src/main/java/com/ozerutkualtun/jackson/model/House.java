b8e3962c1a928e09b878881f09a568010b035a8d
Özer Altun <ozerutkualtun@gmail.com>
2022-08-04 16:25:47 +0300

    Jackson Property Inclusion Annotation #2 : @JsonIgnore

--- src/main/java/com/ozerutkualtun/jackson/model/House.java
+++ src/main/java/com/ozerutkualtun/jackson/model/House.java
@@ -1,6 +1,7 @@
 package com.ozerutkualtun.jackson.model;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,9 +12,10 @@ import java.util.Map;
 @AllArgsConstructor
 @Getter
 @Setter
-@JsonIgnoreProperties(value = {"id", "size"}) // Jackson Property Inclusion Annotation #1
+//@JsonIgnoreProperties(value = {"id", "size"}) // Jackson Property Inclusion Annotation #1
 public class House {
 
+    @JsonIgnore // Jackson Property Inclusion Annotation #1
     private Integer id;
     private Map<String, String> location;
     private Long price;
