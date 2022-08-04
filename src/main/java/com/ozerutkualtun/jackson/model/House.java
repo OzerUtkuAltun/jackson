3bf9947131402af53c89935cdc484b63a8e965b1
Özer Altun <ozerutkualtun@gmail.com>
2022-08-04 16:30:27 +0300

    Jackson Property Inclusion Annotation #3 : @JsonIgnoreType

--- src/main/java/com/ozerutkualtun/jackson/model/House.java
+++ src/main/java/com/ozerutkualtun/jackson/model/House.java
@@ -3,6 +3,7 @@ package com.ozerutkualtun.jackson.model;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,5 +21,14 @@ public class House {
     private Map<String, String> location;
     private Long price;
     private Integer size;
+    private Owner name;
+
+    // Jackson Property Inclusion Annotation #3
+    @JsonIgnoreType // marks all properties of an annotated type to be ignored.
+    @AllArgsConstructor
+    public static class Owner {
+        public String firstName;
+        public String lastName;
+    }
 
 }
